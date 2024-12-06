e2e7ac97a787ff6745628a368115b0a86ffb0ea3
Ruslan Zhabskyi <ruslanzhabskyi@Ruslans-MacBook-Pro.local>
2024-12-06 21:49:04 +0000

    Control Flow

---
+++ bin/if.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// int main() {
+//     int x = 1;
+//     if (x==1)
+//         printf("x is equal to one.\n");
+//     else
+//         printf("For comparison use == as = is the assignment operator.\n");
+//     return 0;
+// }
+
+int main() {
+    int value = 1;
+    while (value<=3)
+    {
+        printf("Value is %d\n",value);
+        value++;
+    }
+    return 0;
+}
