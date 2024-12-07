ac91cd26442d3af33062e155a0fa80159686477c
Ruslan Zhabskyi <ruslanzhabskyi@Ruslans-MacBook-Pro.local>
2024-12-07 10:32:56 +0000

    Input Args 2

--- bin/args.c
+++ bin/args.c
@@ -1,9 +1,29 @@
+// #include <stdio.h>
+
+// int main (int argc, char *argv[]){
+//     int c;
+//     printf("Number of command line argument passed: %d\n", argc);
+//     for (c = 0; c < argc; c++)
+//         printf("%d. Command lineargument passed is %s\n", c + 1, argv[c]);
+//         return 0;
+// }
 #include <stdio.h>
 
-int main (int argc, char *argv[]){
-    int c;
-    printf("Number of command line argument passed: %d\n", argc);
-    for (c = 0; c < argc; c++)
-        printf("%d. Command lineargument passed is %s\n", c + 1, argv[c]);
-        return 0;
+int main(){
+    int array[100], n, c;
+
+    printf("Enter the number of elements in array\n");
+    scanf("%d", &n);
+
+    printf("Enter %d elements\n", n);
+
+    for (c = 0; c < n; c++)
+        scanf("%d", &array[c]);
+
+    printf("Array elements entered by you are:\n");
+
+    for (c = 0; c < n; c++)
+        printf("array[%d] = %d\n", c, array[c]);
+
+    return 0;
 }
