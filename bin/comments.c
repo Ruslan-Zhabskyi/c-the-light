5ac8bb74e80c58e63dd6fad33dc6420fea836e74
Ruslan Zhabskyi <ruslanzhabskyi@Ruslans-MacBook-Pro.local>
2024-12-07 10:43:42 +0000

    Comments

---
+++ bin/comments.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    // Single line comment in c source code
+
+    printf("Writing comments is very useful.\n");
+
+    /*
+     * Multiline comment syntax
+     * Comments help us to understand code later easily.
+     * Will you write comments while developing programs ?
+     */
+
+    printf("Good luck\n");
+
+    return 0;
+}
