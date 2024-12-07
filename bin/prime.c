a2650de860b9a3e2291c09fa1db3c5e02706972a
Ruslan Zhabskyi <ruslanzhabskyi@Ruslans-MacBook-Pro.local>
2024-12-07 10:06:16 +0000

    Prime Numbers

---
+++ bin/prime.c
@@ -0,0 +1,48 @@
+// #include <stdio.h>
+
+// int main(){
+//    int n, c;
+//    printf("Enter a number\n");
+//    scanf("%d", &n);
+//    if ( n == 2 )
+//       printf("Prime number.\n");
+//    else
+//    {
+//        for ( c = 2 ; c <= n - 1 ; c++ )
+//        {
+//            if ( n % c == 0 )
+//               break;
+//        }
+//        if ( c != n )
+//           printf("Not prime.\n");
+//        else
+//           printf("Prime number.\n");
+//    }
+//    return 0;
+// }
+
+#include <stdio.h>
+
+int main(){
+    int n, c;
+    while (1)
+    {
+        printf("Enter a number\n");
+        scanf("%d", &n);
+        if (n == 2)
+            printf("Prime number.\n");
+        else
+        {
+            for (c = 2; c <= n - 1; c++)
+            {
+                if (n % c == 0)
+                    break;
+            }
+            if (c != n)
+                printf("Not prime.\n");
+            else
+                printf("Prime number.\n");
+        }
+    }
+    return 0;
+}
